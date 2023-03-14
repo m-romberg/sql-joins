50c6769dd128738fd65420945dcaa2ae5450b982
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-03-14 12:26:14 -0700

    add queries for 1 and 2

--- queries.sql
+++ queries.sql
@@ -1,1 +1,5 @@
 -- write your queries here
+SELECT * FROM owners FULL JOIN vehicles ON (owners.id = vehicles.owner_id);
+
+SELECT first_name, count (first_name) FROM owners JOIN vehicles ON  (owners.id = vehicles.owner_id) GROUP BY first_name ORDER BY first_name;
+
